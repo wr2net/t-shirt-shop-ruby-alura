d9cd54ab29a452a8b75de2a8c3a86ce850f028d5
wr2net <wagnerr2@gmail.com>
2023-08-16 22:06:10 -0300

    feat: adiciona formulário para cadastrar novos produtos.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,12 @@ class ProductsController < ApplicationController
     @products_list_order_price = Product.order(:price).limit 2
   end
 
+  def create
+    name = params["name"]
+    description = params["description"]
+    quantity = params["quantitiy"]
+    price = params["price"]
+
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "products#index"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
 end
