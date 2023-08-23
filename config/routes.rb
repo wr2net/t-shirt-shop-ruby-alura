dc35a12b76ef348349466037d231a4bd0773140e
wr2net <wagnerr2@gmail.com>
2023-08-23 00:08:12 -0300

    feat: adiciona edição e cira alteração do nome do botão com o recurso locales. feat: refatoração do código

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :departaments
-  resources :products, only: [:new, :create, :destroy]
+  resources :products, only: [:new, :create, :destroy, :edit, :update]
   get "/products/search" => "products#search", as: :search_product
   root "products#index"
 end
