0e5ad3f4a30bceeced8536ccffda05bd019e1fc1
wr2net <wagnerr2@gmail.com>
2023-08-22 22:02:02 -0300

    feat: adicionando validação nos campos e exebindo mensagens de falha ou de sucesso. Adicionado internacionalização.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,15 @@ class ProductsController < ApplicationController
     @products_list_order_price = Product.order(:price).limit 2
   end
 
+  def new
+    @product = Product.new
+  end
+
   def create
-    new_product = params.require(:product).permit :name, :description, :quantitiy, :price
-    product = Product.new new_product
-    if product.save
+    values = params.require(:product).permit :name, :description, :quantitiy, :price
+    @product = Product.new values
+    if @product.save
+      flash[:notice] = "Produto adicionado com sucesso!"
       redirect_to root_url
     else
       render :new
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
 
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :price, presence: true
   validates :quantitiy, presence: true
 
 end
